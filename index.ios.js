625321fd5031b4024b3a11f185c1183558ddeb5e
Wesley Ellis <tahnok@gmail.com>
2015-08-28 08:34:00 -0400

    show button if product is in

--- index.ios.js
+++ index.ios.js
@@ -9,26 +9,105 @@ var {
   AppRegistry,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableHighlight,
   View,
 } = React;
 
+var ProductInStock = React.createClass({
+  propTypes: {
+    product: React.PropTypes.object,
+  },
+  getDefaultProps() {
+    return {
+      product: null,
+    };
+  },
+
+  inStock() {
+    var product = this.props.product;
+    if(product === null) {
+      return false;
+    } else {
+      return product.variants[0].inventory_quantity > 0;
+    }
+  },
+
+  buyNow() {
+
+  },
+
+  render() {
+    if(this.inStock()) {
+      return (
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.buyNow}
+          >
+          <Text style={styles.buttonText}>BUY NOW</Text>
+        </TouchableHighlight>
+        );
+    } else {
+      return <Text>No :(</Text>;
+    }
+  },
+});
+
 var InStockYet = React.createClass({
-  render: function() {
+  getInitialState() {
+    return {
+      text: 'https://realer-dogs.sello.com/products/teapot.json',
+      productJson: null,
+    };
+  },
+
+  onCheckProductPressed() {
+    var productUrl = this.state.text;
+    if(this.validUrl(productUrl)) {
+      this.checkIfProductIsIn(productUrl);
+    }
+  },
+
+  validUrl(productUrl) {
+    return !!productUrl;
+  },
+
+  checkIfProductIsIn(productUrl) {
+    fetch(productUrl)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          productJson: responseData.product,
+        });
+      })
+      .done();
+  },
+
+  render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text style={styles.title}>
+          Is it in stock yet?
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <TextInput
+           style={styles.textInput}
+           autoFocus={true}
+           placeholder="eg: http://myshop.com/product-handle"
+           textAlign="center"
+           onChangeText={(text) => this.setState({text})}
+           value={this.state.text}
+           clearButtonMode="always"
+          />
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.onCheckProductPressed}
+          >
+          <Text style={styles.buttonText}>Is it?</Text>
+        </TouchableHighlight>
+        <ProductInStock product={this.state.productJson} />
       </View>
     );
-  }
+  },
 });
 
 var styles = StyleSheet.create({
@@ -37,16 +116,30 @@ var styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    margin: 24,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  title: {
+    fontSize: 40,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+  },
+  button: {
+    height: 36,
+    flexDirection: 'row',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center',
   },
 });
 
